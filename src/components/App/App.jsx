8ceb2cb0f4e4cf92492316216963166a9dbcb8fd
phiathao <phiat11@gmail.com>
2018-12-09 16:00:18 -0600

    add Mui Grid

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,16 +4,24 @@ import './App.css';
 import Header from '../Header/Header';
 import Feedback from '../Feedback/Feedback';
 import ReFeedback from '../rFeedBack/rFeedBack';
+import Grid from '@material-ui/core/Grid';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <br/>
-        <Feedback />
-        <ReFeedback />
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Header />
+          </Grid>
+          <Grid item xs={8}>
+            <Feedback />
+          </Grid>
+          <Grid item xs={4}>
+            <ReFeedback />
+          </Grid>
+        </Grid>
       </div>
     );
   }

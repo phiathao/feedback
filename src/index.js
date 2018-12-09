6be98a4fd9ceb8c21c1b1679943f44e9c1da9fe3
phiathao <phiat11@gmail.com>
2018-12-09 14:41:08 -0600

    init git and dependency

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App/App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
